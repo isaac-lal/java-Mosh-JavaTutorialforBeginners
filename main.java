acac0322c48e843149b08469a67983663dcf9ff4
isaaclal124 <isaaclal124@gmail.com>
2022-08-11 14:58:04 -0400

    added java files

---
+++ main.java
@@ -0,0 +1,12 @@
+// Name: Isaac Lal
+
+class main 
+{
+    public static void main(String[] args) 
+    {
+        // static = main settings
+        // void = not returning a value
+        
+        System.out.println("Hello World");
+    }
+}
